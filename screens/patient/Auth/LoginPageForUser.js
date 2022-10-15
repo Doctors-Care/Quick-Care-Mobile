797442f7c708daed9d636aa1cfe52a15b21581ce
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-10-15 20:03:04 +0100

    setting async Storage in the front and correcting the way we passed the state

--- screens/patient/Auth/LoginPageForUser.js
+++ screens/patient/Auth/LoginPageForUser.js
@@ -1,45 +1,66 @@
-import { StyleSheet, TextInput, View, Image, TouchableOpacity, Text, Pressable, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  Pressable,
+  ScrollView,
+} from "react-native";
 import React, { useState } from "react";
 import { useTogglePasswordVisibility } from "../../../hooks/TogglePassword";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import axios from "axios";
-import LottieView from 'lottie-react-native';
+import LottieView from "lottie-react-native";
 import link from "../../../Adress";
-
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function LoginPageForUser({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { passwordVisibility, rightIcon, handlePasswordVisibility } = useTogglePasswordVisibility();
+  const { passwordVisibility, rightIcon, handlePasswordVisibility } =
+    useTogglePasswordVisibility();
   const [message, setMessage] = useState("");
 
-
-
   const getData = async () => {
-    const res = await axios.get('https://geolocation-db.com/json/')
-  }
-  const login = () => {
+    const res = await axios.get("https://geolocation-db.com/json/");
+  };
+  const patientStore = async (patient) => {
+    try {
+      await AsyncStorage.setItem("Patient",JSON.stringify(patient));
+     console.log(patient)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const login =  () => {
     const user = {
       email: email,
-      password: password
-    }
+      password: password,
+    };
     getData();
 
-    axios.post(`${link}/user/signin`, user).then((ok) => {
-      setMessage("Welcome");
-      console.log(ok.data.id)
-      navigation.navigate("EmergencyHome", { id: ok.data.id })
-    }).catch((err) => { console.log(err); setMessage("wrong entries") })
-  }
+    axios
+      .post(`${link}/user/signin`, user)
+      .then((ok) => {
+        setMessage("Welcome");
+        patientStore(ok.data)
+        navigation.navigate("EmergencyHome", { id: ok.data.id });
+      })
+      .catch((err) => {
+        console.log(err);
+        setMessage("wrong entries");
+      });
+  };
 
   return (
     <ScrollView style={styles.out}>
-
       <LottieView
         style={styles.logo}
         source={require("../../../assets/64216-super-nurse-animation.json")}
-        autoPlay />
+        autoPlay
+      />
       <View style={styles.container}>
         <View style={styles.inputView}>
           <TextInput
@@ -59,27 +80,26 @@ export default function LoginPageForUser({ navigation }) {
               onChangeText={(password) => setPassword(password)}
             ></TextInput>
             <Pressable onPress={handlePasswordVisibility}>
-              <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
+              <MaterialCommunityIcons
+                name={rightIcon}
+                size={22}
+                color="#232323"
+              />
             </Pressable>
           </View>
         </View>
         <TouchableOpacity>
           <Text style={styles.forgot_button}>Forgot Password?</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.loginBtn}
-          onPress={() => login()}
-        >
+        <TouchableOpacity style={styles.loginBtn} onPress={() => login()}>
           <Text style={styles.loginText}>Login</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
-
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -96,8 +116,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderColor: "#077871",
     borderWidth: 2,
-
-
   },
 
   TextInput: {
@@ -106,8 +124,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginLeft: 20,
     marginTop: 100,
-
-
   },
   forgot_button: {
     height: 30,
@@ -129,14 +145,13 @@ const styles = StyleSheet.create({
     height: 200,
     top: 10,
     borderRadius: 0,
-    left: 40
+    left: 40,
   },
   inputViewPassword: {
-    display: 'flex',
-    flexDirection: 'row',
-
+    display: "flex",
+    flexDirection: "row",
   },
   loginText: {
-    color: "#ffffff"
-  }
+    color: "#ffffff",
+  },
 });
